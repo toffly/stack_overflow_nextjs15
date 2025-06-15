85fada3613adbe134291faa4a6dde4cf6e317aa0
toffly <toffly@outlook.com>
2025-06-15 19:10:21 +0700

    implement UI question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
+import QuestionForm from "@/components/forms/QuestionForm";
 import React from "react";
 
 const AskAQuestion = () => {
-  return <div>AskAQuestion</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900"> Ask a question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
 export default AskAQuestion;
